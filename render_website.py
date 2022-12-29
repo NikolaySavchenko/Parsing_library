d04300c11eacc3d11945ea25ba8fdeb731b8508b
Nikolay Savchenko <7548568@gmail.com>
2022-12-29 22:37:54 +0300

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,36 +1,45 @@
 import json
-from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-template = env.get_template('template.html')
 
-with open("library/about_books.json", "r", encoding='utf8') as books:
-    books_json = books.read()
-about_books = json.loads(books_json)
-books = []
-for about_book in about_books:
-    if about_book['image'] != 'Обложка отсутствует':
-        book = {
-            'image': about_book['image'].replace('\\', '/'),
-            'title': about_book['title'],
-            'author': about_book['author'],
-        }
-    else:
-        book = {
-            'image': 'nopic.gif',
-            'title': about_book['title'],
-            'author': about_book['author'],
-        }
-    books.append(book)
+def rebuild():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
 
-rendered_page = template.render(books=books)
+    with open("library/about_books.json", "r", encoding='utf8') as books:
+        books_json = books.read()
+    about_books = json.loads(books_json)
+    books = []
+    for about_book in about_books:
+        if about_book['image'] != 'Обложка отсутствует':
+            book = {
+                'image': about_book['image'].replace('\\', '/'),
+                'title': about_book['title'],
+                'author': about_book['author'],
+            }
+        else:
+            book = {
+                'image': 'nopic.gif',
+                'title': about_book['title'],
+                'author': about_book['author'],
+            }
+        books.append(book)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    rendered_page = template.render(books=books)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+    print("Site rebuilt")
+
+
+rebuild()
+
+server = Server()
+
+server.watch('template.html', rebuild)
+
+server.serve(root='.')
