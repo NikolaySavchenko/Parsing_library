93061f9ebd5ff8b4df1f2bc49f7e6f57333500c4
Nikolay Savchenko <7548568@gmail.com>
2023-01-03 20:38:55 +0300

    Add chusing json file path

--- render_website.py
+++ render_website.py
@@ -1,20 +1,20 @@
 import json
+import argparse
 from more_itertools import chunked
 from pathlib import Path
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 
 
-def rebuild():
+def rebuild(db_path):
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template('template.html')
 
-    with open("library/about_books.json", "r", encoding='utf8') as books:
-        books_json = books.read()
-    about_books = json.loads(books_json)
+    with open(db_path, "r", encoding='utf8') as books:
+        about_books = json.load(books)
     books = []
     for about_book in about_books:
         if about_book['image'] != 'Обложка отсутствует':
@@ -49,10 +49,15 @@ def rebuild():
             file.write(rendered_page)
 
 
-rebuild()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Input DB file path')
+    parser.add_argument('db_path', nargs='?', type=str, default="library/about_books.json")
+    db_path = parser.parse_args().db_path
 
-server = Server()
+    rebuild(db_path)
 
-server.watch('template.html', rebuild)
+    # server = Server()
 
-server.serve(root='.')
+    # server.watch('template.html', rebuild)
+
+    # server.serve(root='.')
